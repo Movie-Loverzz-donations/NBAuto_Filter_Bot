f2bae58ebd4dedf620e1b184dfbeb15b22d6e5a4
Movie-Loverzz-donations <Msmarvel11220@gmail.com>
2025-05-25 22:37:37 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,7 +51,7 @@ DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://cluster:yZ8tvosjuklg
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+7hhFYFo61m5hNzU9')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/movie_loverzz')
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/mladminbot')
-UPDATE_CHANNEL_LNK = environ.get('UPDATE_CHANNEL_LNK', 'https://t.me/')
+UPDATE_CHANNEL_LNK = environ.get('UPDATE_CHANNEL_LNK', 'https://t.me/movie_loverzz')
 
 #Force Subscription Channel (Put Same Channel Id In Both Veriables)
 AUTH_CHANNEL = int(environ.get('AUTH_CHANNEL', '-1001622914589')) 
@@ -124,7 +124,7 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else "https://{}/".format(FQDN, PORT)
+URL = "https://movie-loverzz-f2c1e32890d7.herokuapp.com/".format(FQDN) if ON_HEROKU or NO_PORT else "https://movie-loverzz-f2c1e32890d7.herokuapp.com/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'SilentXBotz'))
@@ -138,9 +138,9 @@ else:
     ON_HEROKU = False
 HAS_SSL = bool(getenv('HAS_SSL', True))
 if HAS_SSL:
-    URL = "https://{}/".format(FQDN)
+    URL = "https://movie-loverzz-f2c1e32890d7.herokuapp.com/".format(FQDN)
 else:
-    URL = "http://{}/".format(FQDN)
+    URL = "https://movie-loverzz-f2c1e32890d7.herokuapp.com/".format(FQDN)
 
 
 REACTIONS = ["🤝", "😇", "🤗", "😍", "👍", "🎅", "😐", "🥰", "🤩", "😱", "🤣", "😘", "👏", "😛", "😈", "🎉", "⚡️", "🫡", "🤓", "😎", "🏆", "🔥", "🤭", "🌚", "🆒", "👻", "😁"]
